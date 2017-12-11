7a03e28259fee16a48bea4c23ae7e35a60d3c153
M.Vainauskas <mindelis1@gmail.com>
2017-12-11 00:06:01 +0000

    Started work on User Authentication

---
+++ LittleNotebook/LittleNotebook/Authentication/AuthenticationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleNotebook.Authentication
+{
+    public interface IAuthenticationService
+    {
+        User AuthenticateUser(string username, string password);
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private class InternalUserInfo
+        {
+            public InternalUserInfo(string uName, string email, string password)
+            {
+                UserName = uName;
+                Email = email;
+                Password = password;
+            }
+
+            public string Email { get; private set; }
+            public string Password { get; private set; }
+            public string UserName { get; private set; }
+
+            private static readonly List<InternalUserInfo> _users = new List<InternalUserInfo>()
+        {
+            new InternalUserInfo("Mark", "mark@company.com",     "MB5PYIsbI2YzCUe34Q5ZU2VferIoI4Ttd+ydolWV0OE="),
+            new InternalUserInfo("John", "john@company.com", "hMaLizwzOQ5LeOnMuj+C6W75Zl5CXXYbwDSHWW9ZOXc=")
+        };
+
+            public User AuthenticateUser(string username, string clearTextPassword)
+            {
+                InternalUserInfo userData = _users.FirstOrDefault(u => u.Username.Equals(username)
+                    && u.HashedPassword.Equals(CalculateHash(clearTextPassword, u.Username)));
+                if (userData == null)
+                    throw new UnauthorizedAccessException("Access denied. Please provide some valid credentials.");
+
+                return new User(userData.Username, userData.Email, userData.Roles);
+            }
+        }
+    }
+}
---
+++ LittleNotebook/LittleNotebook/Authentication/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleNotebook.Authentication
+{
+    //User model class for user login details.
+    public class User : IIdentity
+    {
+        public User(string uName, string email, string password)
+        {
+            Name = uName;
+            Email = email;
+            Password = password;
+        }
+
+        public User(string userName, string email)
+        {
+            Name = userName;
+            Email = email;
+        }
+
+        //Have to implement the Name interface
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+
+        //This method checks if user is authenticated
+        public bool IsAuthenticated { get { return !string.IsNullOrEmpty(Name); } }
+
+        //Implementing interface required by Security.Principal
+        public string AuthenticationType
+        {
+            get { return "Notebook Authentication"; }
+        }
+
+    }
+}
